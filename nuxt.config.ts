524606f14ba39422307b9ab206da82b543084e0d
sttshka <sttshka.dev@gmail.com>
2025-11-08 08:16:05 +0300

    deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', '@netlify/nuxt'],
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [tailwindcss()],
